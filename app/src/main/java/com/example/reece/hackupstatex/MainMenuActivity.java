2e5b6db1ebda90a8ace46f5180ff09b8312e5d3d
Carter Rollins <rollinc@clarkson.edu>
2017-10-08 02:50:40 -0400

    Fixed UI ish

--- app/src/main/java/com/example/reece/hackupstatex/MainMenuActivity.java
+++ app/src/main/java/com/example/reece/hackupstatex/MainMenuActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainMenuActivity extends AppCompatActivity {
@@ -14,13 +15,17 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        configureNextButton();
     }
 
-    public void sendMessage(View view){
-        Intent intent = new Intent(this, StartNewGameActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editTextTitle);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    public void configureNextButton(){
+        Button nextButton = (Button) findViewById(R.id.buttonStart);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenuActivity.this, StartNewGameActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/reece/hackupstatex/StartNewGameActivity.java
+++ app/src/main/java/com/example/reece/hackupstatex/StartNewGameActivity.java
@@ -3,6 +3,8 @@ package com.example.reece.hackupstatex;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class StartNewGameActivity extends AppCompatActivity {
@@ -11,13 +13,15 @@ public class StartNewGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_new_game);
+    }
 
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainMenuActivity.EXTRA_MESSAGE);
-
-        // Capture the layout's TextView and set the string as its text
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(message);
+    public void configureBackButton(){
+        Button backButton = (Button) findViewById(R.id.buttonBack);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
