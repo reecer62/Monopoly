ddc11ff6467edd341d327e326eb4c93908a7588d
reecer62 <reece.emero98@gmail.com>
2017-10-09 17:42:56 -0400

    Added some networking that needs to be changed

---
+++ app/src/main/java/Networking/ConnectToHost.java
@@ -0,0 +1,9 @@
+package Networking;
+
+/**
+ * Created by Reece on 10/9/2017.
+ */
+
+public class ConnectToHost {
+
+}
--- app/src/main/java/com/example/reece/hackupstatex/LobbyActivity.java
+++ app/src/main/java/com/example/reece/hackupstatex/LobbyActivity.java
@@ -5,9 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class LobbyActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Bundle bundle = new Bundle();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         String hostName;
@@ -18,7 +25,7 @@ public class LobbyActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_lobby);
 
 
-        Bundle bundle = getIntent().getExtras();
+        bundle = getIntent().getExtras();
         if (!bundle.isEmpty()) {
             if (bundle.containsKey("hostName") && bundle.containsKey("numPlayers") && bundle.containsKey("lobbyName")) {
                 hostName = bundle.getString("hostName");
@@ -65,7 +72,28 @@ public class LobbyActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case  R.id.buttonRunGame: {
-                //Calls createAGame only called once per client because each connects only once
+                //Calls connectToHost, only called once per client because each connects only once
+
+                //Get host name from user
+                String host_Name = bundle.getString("hostName");
+                //Port number that is going to be used
+                int port = 7001;
+                try {
+                    //Open a socket
+                    Socket skt = new Socket(host_Name, port);
+                    //Open an output stream to the socket
+                    PrintWriter outToServer = new PrintWriter(skt.getOutputStream(), true); //Sets autoFlush() to true so you don't have to flush the output stream every time you print to server
+                    //Open an input stream from the socket
+                    BufferedReader inFromServer = new BufferedReader(new InputStreamReader(skt.getInputStream()));
+                } catch (UnknownHostException uhe) {
+                    //Can't find host
+                    System.err.println("Don't know the h*ckin host " + host_Name);
+                    System.exit(1);
+                } catch (IOException ioe) {
+                    //IO error
+                    System.err.println("I/O error in connecting to " + host_Name);
+                    System.exit(1);
+                }
                 break;
             }
 
